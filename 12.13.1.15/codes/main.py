3dfd032513faa66a8ae909739aae8bc265034c2a
Zeus Crusher <140877971+barathsurya2004@users.noreply.github.com>
2023-08-21 11:41:14 +0530

    Add files via upload

--- 12.13.1.15/codes/main.py
+++ 12.13.1.15/codes/main.py
@@ -1,7 +1,8 @@
 import numpy as np 
 a=np.array([[1/3,2/3,0,0],[0,0,1/2,1/2],[0,0,1,0],[0,0,0,1]]) 
 b=np.array([1,0,0,0]) 
-m=1000
-c=np.linalg.matrix_power(a,m)
+c=a 
+for i in range(100): 
+    c=np.matmul(c,a) 
 print(np.matmul(b,c))
 
