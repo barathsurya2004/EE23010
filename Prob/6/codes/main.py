ebcac11d71a80d2c487f152189d6f07d3213e8c4
_zeus_crusher_ <barathsurya2004@gmail.com>
2023-09-11 09:28:36 +0530

    code updated

--- Prob/6/codes/main.py
+++ Prob/6/codes/main.py
@@ -1,10 +1,21 @@
-import scipy.stats as stats
+import math
+
+def binomial_distribution(p, n, z):
+  probabilities = []
+  for k in range(n + 1):
+    probabilities.append(math.comb(n,k)*(math.pow(p, k) * math.pow(1 - p, n - k)) * math.pow(z, k))
+
+  return probabilities
 
 
-n = 3 
-p = 0.5  
-X = stats.binom(n, p)
-pmf_values = [X.pmf(k) for k in range(n + 1)]
+p = 1/2
+n = 3
+z = 1
 
-for k, pmf in enumerate(pmf_values):
-    print(f'P(X = {k}) = {pmf:.3f}')
+probabilities = binomial_distribution(p, n, z)
+
+print("Probability of getting 3 heads:", probabilities[3])
+print("Probability of getting 2 heads:", probabilities[2])
+print("Probability of getting 1 head:", probabilities[1])
+print("Probability of getting 0 heads:", probabilities[0])
+
