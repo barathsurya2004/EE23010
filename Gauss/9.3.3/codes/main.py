e7f00dfbc8a62ac8cfd92e4c526df01392ba5585
_zeus_crusher_ <barathsurya2004@gmail.com>
2023-09-22 14:40:09 +0530

    codes and figs

--- Gauss/9.3.3/codes/main.py
+++ Gauss/9.3.3/codes/main.py
@@ -1,18 +1,28 @@
+from scipy.stats import norm
+from scipy.stats import binom
 import numpy as np
+from matplotlib import pyplot as plt
 
-def simulate_sample_numpy(prob_defective, sample_size, num_simulations):
-    samples = np.random.choice([0, 1], size=(num_simulations, sample_size), p=[1 - prob_defective, prob_defective])
-    num_defective = np.sum(samples, axis=1)
-    num_successes = np.sum(num_defective <= 1)
-    
-    probability = num_successes / num_simulations
-    return probability
+p = 0.05
+n = 10
+k = 1
+sig = np.sqrt(n*p*(1-p))
+print("Probability from Gaussian approximation: ",norm.cdf((k-n*p+0.5)/sig))
+print("Probability from Binomial: ",(binom.cdf(k,n,p)))
 
-# Parameters
-prob_defective = 0.05  # 5% defective items
-sample_size = 10       # Size of the sample
-num_simulations = 100000  # Number of simulations
-
-result = simulate_sample_numpy(prob_defective, sample_size, num_simulations)
-print(f"simulated probability of not more than one defective item: {result:.4f}")
-
+n = 10
+p = 0.05
+sig = np.sqrt(n*p*(1-p))
+k = np.linspace(0,n,n+1)
+fig, ax = plt.subplots()
+xpoints = k
+ypoints = np.array(binom.pmf(k,n,p))
+ax.stem(xpoints, ypoints,linefmt='r-', markerfmt='ro', basefmt='k--')
+xpoints = np.linspace(-n,n,100*n)
+ypoints = np.array(norm(n*p,sig).pdf(xpoints))
+ax.plot(xpoints, ypoints,'b')
+plt.legend(["Gaussian","Binomial"])
+plt.xlabel("Number of Successes")
+plt.ylabel("Probability")
+plt.grid()
+plt.show()
