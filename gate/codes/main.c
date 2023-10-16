97c16bd103bf49909ab7553ce0ff6e68fbe2880b
_zeus_crusher_ <barathsurya2004@gmail.com>
2023-10-16 19:04:53 +0530

    comments added to code

--- gate/codes/main.c
+++ gate/codes/main.c
@@ -4,7 +4,7 @@
 #include <time.h>
 
 int main() {
-    double lambda = 8; // Change lambda to your desired mean
+    double l = 9; // Change l to your desired mean
 
     int n = 100000; // Number of simulations
     int validCount = 0; // Count of valid X values (X > 0)
@@ -13,33 +13,35 @@ int main() {
     srand(time(0)); // Seed the random number generator
 
     for (int i = 0; i < n; i++) {
-        int X = 0;
-        double p = exp(-lambda);
-        double F = p;
+        double el = exp(-l);  //defining e^l
+        double pp = el;         //initialising p(X=0)
+        double u = (double)rand() / RAND_MAX;   //generating uniform random variable
+        int X = 0;          //initialising poisson random variable
+        double fact = 1;    //(numerator)factorial part in pmf
+        double pow = 1;     //(denominator)power part in pmf
 
-        double U = (double)rand() / RAND_MAX;
-
-        while (U > F) {
-            p *= lambda / (X + 1);
-            F += p;
+        while (u > pp) { //checking which part of poisson distribution urv is lieing in 
             X++;
+            fact *= X; //updating factorial and powers according to the poisson distribution
+            pow *= l;
+            pp += (pow / fact) * el; //going up poisson random variable by one step every time
         }
-
-        if (X > 0) {
+//when found out the poisson random variable in which urv was lieing in we break out of the loop
+        if (X > 0) { //checking the question conditions
             double Y = 1.0 / (X + 1);
-            sumY += Y;
+            sumY += Y; //for finding mean = sum/count
             validCount++;
         }
     }
 
     if (validCount > 0) {
-        double conditionalExpectation = sumY / validCount;
+        double conditionalExpectation = sumY / validCount; //simulated mean
         printf("Simulated E(1 / (X + 1) | X > 0): %lf\n", conditionalExpectation);
     } else {
         printf("No valid X values (X > 0) were generated.\n");
     }
 
-    double formulaans = (1 - exp(-lambda) - lambda * exp(-lambda)) / (lambda * (1 - exp(-lambda)));
+    double formulaans = (1 - exp(-l) - l * exp(-l)) / (l * (1 - exp(-l))); //formula mean we found by theory
     printf("Formula-based answer: %lf\n", formulaans);
 
     return 0;
